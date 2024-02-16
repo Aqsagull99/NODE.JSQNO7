7b132b21d384174d05e1fc5fa3f35fb1166eac46
Aqsagull99 <159878357+Aqsagull99@users.noreply.github.com>
2024-02-17 00:23:51 +0500

    Add files via upload

---
+++ qno7.js
@@ -0,0 +1,21 @@
+// // QNO:7
+// // Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+// // addition
+// let num1:number =4;
+// let num2:number =4;
+// console.log(num1 + num2)
+// // // subtraction
+// // let num3:number =12;
+// // let num4:number =4;
+// // // console.log(num1 - num2)
+// // // mulplication
+// // let num5:number =2;
+// // let num6:number =4;
+// // // console.log(num1 * num2)
+// // // devision
+// // let num7:number =8;
+// // let num8:number =1;
+// // console.log(num1 / num2)
+// // console.log(num3 * num4)
+// // console.log(num5 + num6)
+// // console.log(num7 - num8)
